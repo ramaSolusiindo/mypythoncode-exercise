7a15912008ce9fd5d41ac348cb58de5d4d587ee3
bo1boiX <rama.as.bht@gmail.com>
2022-08-06 06:31:21 +0700

    new fil

---
+++ Python Crash Course/Chap9-class1-dog.py
@@ -0,0 +1,19 @@
+# dog.py
+class Dog:
+    # A simple attempt to model a dog
+    def __init__(self,name,age):
+        self.name = name
+        self.age = age
+
+    def sit(self):
+        print(f"{self.name} is now sitting")
+
+    def rollover(self):
+        print(f"{self.name} rolled over!")
+
+my_dog = Dog('Willie', 6)
+
+print(f"My dog's name is : {my_dog.name}.")
+print(f"My dog is {my_dog.age} years old")
+
+
