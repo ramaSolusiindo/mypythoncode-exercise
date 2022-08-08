7d2b97a9af5e131c5b60fe7495a3aeed2dad9a93
bo1boiX <rama.as.bht@gmail.com>
2022-08-09 05:47:33 +0700

    append new changes

---
+++ Python Crash Course/Chap10-FilesExection-file-reader.py
@@ -0,0 +1,39 @@
+"""
+ exercise read files
+"""
+filenames = 'Chap10-FilesExecption-pi_digit.txt'
+with open(filenames) as file_object:
+    lines = file_object.readlines()
+
+with open(filenames) as file_object:
+    baca = file_object.read()
+
+with open(filenames) as file_object:
+    baca2 = file_object.readline()
+
+
+print(f"This is readlines result : {lines}")
+print(f"This is read result : \n{baca}")
+print(f"This is readlines result : {baca2}")
+
+pi_string = ''
+for line in lines:
+    # pi_string += line.rstrip()
+    pi_string += line.strip()
+
+print(pi_string)
+print(len(pi_string))
+
+
+"""
+with open('Chap10-FilesExecption-pi_digit.txt') as file_object:
+    contents = file_object.read()
+
+filepath = 'C:/Users/Lenovo T470s/Documents/seskoTNI.txt'
+with open(filepath) as file_object2 :
+    contents2 = file_object2.read()
+# print(contents)
+# remove the blanklines
+# print(contents.rstrip())
+print(contents2)
+"""
---
+++ Rwid-Exercise/fundamental3-while.py
@@ -0,0 +1,13 @@
+"""
+ Perulangan dengan while
+"""
+total_book = 10
+total_book_reads = 0
+
+while total_book_reads < total_book:
+    total_book_reads += 1
+    print(f"Read book {total_book_reads}")
+
+print(f"Total book have been read : {total_book_reads}")
+
+
